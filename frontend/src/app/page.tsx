ba9560444f70b62cabb1ae75dcec0e94b4a57bb8
gmoralrubio <gmoralrubio@gmail.com>
2026-09-24 17:52:01 +0200

    feat: actualizados metadata

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>Welldone</h1>
+      <p>Proyecto final KC web 20</p>
     </div>
   );
 }
